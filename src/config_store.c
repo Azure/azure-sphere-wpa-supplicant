bbca6a6a434bd9a60b6c452d9ced0459eb584e3d
Hardik Garg <hargar@microsoft.com>
2020-06-10 18:49:47 -0700

    strcmp is replaced with strncmp

--- src/config_store.c
+++ src/config_store.c
@@ -20,8 +20,7 @@ static char *AppendString(const char *front, const char *back)
     size_t front_len = strlen(front);
     size_t back_len = strlen(back);
     char *dst = malloc(front_len + back_len + sizeof('\0'));
-    if (dst != NULL)
-    {
+    if (dst != NULL) {
         strncpy(&dst[0], front, front_len);
         strncpy(&dst[front_len], back, back_len);
         dst[front_len + back_len] = '\0';
@@ -37,19 +36,15 @@ static size_t GetDistance(const ConfigStoreKvpHeader *p, const ConfigStoreKvpHea
 
 size_t ConfigStore_GetKvpFullSize(const ConfigStoreKvpHeader *p, const ConfigStoreKvpHeader *pEnd)
 {
-    if (!p)
-    {
+    if (!p) {
         return 0;
     }
 
     size_t avail_size = GetDistance(p, pEnd);
 
-    if (p->size <= avail_size)
-    {
+    if (p->size <= avail_size) {
         return p->size;
-    }
-    else
-    {
+    } else {
         return avail_size;
     }
 }
@@ -64,23 +59,17 @@ ConfigStoreKvpHeader *ConfigStore_GetNextKvp(const ConfigStoreKvpHeader *p,
                                              const ConfigStoreKvpHeader *pEnd)
 {
     size_t dist;
-    if (!p)
-    {
+    if (!p) {
         dist = 0;
-    }
-    else if (ConfigStore_CanDereferenceKvp(p, pEnd))
-    {
+    } else if (ConfigStore_CanDereferenceKvp(p, pEnd)) {
         dist = p->size;
-    }
-    else
-    {
+    } else {
         dist = GetDistance(p, pEnd);
     }
 
     ConfigStoreKvpHeader *retval = (ConfigStoreKvpHeader *)((ptrdiff_t)p + dist);
-    if (!ConfigStore_CanDereferenceKvp(retval, pEnd))
-    {
-        retval = (ConfigStoreKvpHeader *)pEnd;
+    if (!ConfigStore_CanDereferenceKvp(retval, pEnd)) {
+        retval = (ConfigStoreKvpHeader *) pEnd;
     }
 
     return retval;
@@ -90,11 +79,9 @@ uint32_t ConfigStore_AddCrc(uint32_t init, const uint8_t *data, size_t size)
 {
     uint32_t crc = init;
     const uint8_t *last = data + size;
-    while (data != last)
-    {
+    while (data != last) {
         crc = crc ^ *data++;
-        for (int j = 7; j >= 0; --j)
-        {
+        for (int j = 7; j >= 0; --j) {
             uint32_t mask = -(crc & 1);
             crc = (crc >> 1) ^ (0xEDB88320 & mask);
         }
@@ -118,15 +105,13 @@ void DeleteFileHelper(struct dirent *file, char *filePath)
     //of character c in string s, or a NULL pointer if c does not occur in the string.
     ptr = rindex(fileName, '.');
 
-    printf("\nthe ptr is: %s\n", ptr);
     //Check for filename extensions
-    if ((ptr != NULL) && (strncmp(ptr, ".conf", 5) == 0))
+    if ((ptr != NULL) && (strncmp(ptr, ".tmp", 4) == 0))
     {
         char *fileToDelete = AppendString(filePath, fileName);
 
         //delete the file
         unlink(fileToDelete);
-        printf("\nthe file is deleted: %s\n", fileName);
         //free the memory
         free(fileToDelete);
     }
@@ -155,8 +140,7 @@ void DeleteAllTempFiles(const char *dirPath)
 
 void ConfigStore_Close(ConfigStore *p)
 {
-    if (p->_fd >= 0)
-    {
+    if (p->_fd >= 0) {
         close(p->_fd);
     }
     free(p->_primary_path);
@@ -167,8 +151,7 @@ void ConfigStore_Close(ConfigStore *p)
 
 void ConfigStore_Move(ConfigStore *pDst, ConfigStore *pSrc)
 {
-    if (pDst != pSrc)
-    {
+    if (pDst != pSrc) {
         ConfigStore_Close(pDst);
         memcpy(pDst, pSrc, sizeof(*pDst));
         ConfigStore_Init(pSrc);
@@ -177,8 +160,7 @@ void ConfigStore_Move(ConfigStore *pDst, ConfigStore *pSrc)
 
 int ConfigStore_ReserveCapacity(ConfigStore *p, size_t capacity)
 {
-    if (capacity > p->_max_size)
-    {
+    if (capacity > p->_max_size) {
         // Can't grow the file beyond max size.
         errno = E2BIG;
         return -1;
@@ -186,11 +168,9 @@ int ConfigStore_ReserveCapacity(ConfigStore *p, size_t capacity)
 
     size_t current_capacity = p->_capacity - p->_begin;
 
-    if (capacity > current_capacity)
-    {
+    if (capacity > current_capacity) {
         uint8_t *new_begin = realloc(p->_begin, capacity);
-        if (new_begin == NULL)
-        {
+        if (new_begin == NULL) {
             return -1;
         }
 
@@ -211,8 +191,7 @@ static bool ConfigStore_InvariantsCheck(const ConfigStore *p)
 
 static bool ReplicaTypeIsValid(ConfigStoreReplicaType rtype)
 {
-    switch (rtype)
-    {
+    switch (rtype) {
     case ConfigStoreReplica_None ... ConfigStoreReplica_Swap:
         return true;
     default:
@@ -223,8 +202,7 @@ static bool ReplicaTypeIsValid(ConfigStoreReplicaType rtype)
 static int Impl_Open(ConfigStore *p, const char *base_filepath, size_t max_size, int flags,
                      ConfigStoreReplicaType rtype)
 {
-    if (!ReplicaTypeIsValid(rtype))
-    {
+    if (!ReplicaTypeIsValid(rtype)) {
         errno = EINVAL;
         return -1;
     }
@@ -233,16 +211,13 @@ static int Impl_Open(ConfigStore *p, const char *base_filepath, size_t max_size,
     p->_max_size = max_size;
 
     p->_primary_path = strdup(base_filepath);
-    if (p->_primary_path == NULL)
-    {
+    if (p->_primary_path == NULL) {
         return -1;
     }
 
-    if (p->_replica_type == ConfigStoreReplica_Swap)
-    {
+    if (p->_replica_type == ConfigStoreReplica_Swap) {
         p->_replica_path = AppendString(base_filepath, ".tmp");
-        if (p->_replica_path == NULL)
-        {
+        if (p->_replica_path == NULL) {
             return -1;
         }
 
@@ -255,8 +230,7 @@ static int Impl_Open(ConfigStore *p, const char *base_filepath, size_t max_size,
     flags |= O_CLOEXEC;
 
     p->_fd = open(p->_primary_path, flags, S_IRUSR | S_IWUSR);
-    if (p->_fd < 0)
-    {
+    if (p->_fd < 0) {
         return -1;
     }
 
@@ -264,16 +238,14 @@ static int Impl_Open(ConfigStore *p, const char *base_filepath, size_t max_size,
 
     int lockmode = read_only ? (LOCK_SH | LOCK_NB) : (LOCK_EX | LOCK_NB);
 
-    if (flock(p->_fd, lockmode) < 0)
-    {
+    if (flock(p->_fd, lockmode) < 0) {
         return -1;
     }
 
     bool ok = false;
     off_t ssize = lseek(p->_fd, 0, SEEK_END);
     ok = (ssize >= 0) && (lseek(p->_fd, 0, SEEK_SET) == 0);
-    if (!ok)
-    {
+    if (!ok) {
         return -1;
     }
 
@@ -281,49 +253,40 @@ static int Impl_Open(ConfigStore *p, const char *base_filepath, size_t max_size,
     bool is_new = (size == 0);
     bool expects_new = (flags & (O_CREAT | O_TRUNC));
 
-    if (is_new)
-    {
-        if (!expects_new)
-        {
+    if (is_new) {
+        if (!expects_new) {
             errno = ENOENT;
             return -1;
         }
         size = sizeof(ConfigStoreFileHeader);
     }
 
-    if (size < sizeof(ConfigStoreFileHeader))
-    {
+    if (size < sizeof(ConfigStoreFileHeader)) {
         errno = ERANGE;
         return -1;
     }
 
-    if (ConfigStore_ReserveCapacity(p, size))
-    {
+    if (ConfigStore_ReserveCapacity(p, size)) {
         return -1;
     }
 
     ConfigStoreFileHeader *header = (ConfigStoreFileHeader *)(p->_begin);
 
-    if (is_new)
-    {
+    if (is_new) {
         // For new files, start with a basic header.
         header->header.size = sizeof(ConfigStoreFileHeader);
         header->header.key = ConfigStoreFileHeaderKey;
         header->signature = ConfigStoreFileSignature;
         header->version = ConfigStoreFileVersion;
         p->_end += sizeof(ConfigStoreFileHeader);
-    }
-    else
-    {
+    } else {
         // For existing files, try to read the store from them.
-        if (read(p->_fd, p->_begin, size) != ssize)
-        {
+        if (read(p->_fd, p->_begin, size) != ssize) {
             return -1;
         }
 
         size_t content_size = ConfigStore_ValidateFormat(p->_begin, size);
-        if (content_size == 0)
-        {
+        if (content_size == 0) {
             // Invalid content.
             errno = EINVAL;
             return -1;
@@ -332,14 +295,12 @@ static int Impl_Open(ConfigStore *p, const char *base_filepath, size_t max_size,
         bool must_truncate =
             !read_only && (content_size < size) && (p->_replica_type != ConfigStoreReplica_Swap);
 
-        if (must_truncate)
-        {
+        if (must_truncate) {
             // The content is valid, but it's shorter than the file. The previous writer may have
             // crashed after it wrote the content but before it truncated the file, so truncate it
             // now.
 
-            if (ftruncate(p->_fd, content_size) != 0)
-            {
+            if (ftruncate(p->_fd, content_size) != 0) {
                 return -1;
             }
 
@@ -364,14 +325,12 @@ int ConfigStore_StatVfs(const char *path, struct statvfs *buf)
 /// <returns> The adjust size on success, or zero on failure. </summary>
 static size_t AdjustedMaxFileSize(const char *file_path, size_t file_size)
 {
-    if (file_size <= ConfigStoreOverheadPerStorageBlock)
-    {
+    if (file_size <= ConfigStoreOverheadPerStorageBlock) {
         return 0;
     }
 
     char *path_copy = strdup(file_path);
-    if (path_copy == NULL)
-    {
+    if (path_copy == NULL) {
         return 0;
     }
 
@@ -380,8 +339,7 @@ static size_t AdjustedMaxFileSize(const char *file_path, size_t file_size)
     int r = ConfigStore_StatVfs(dirname(path_copy), &stat_buf);
     free(path_copy);
 
-    if (r)
-    {
+    if (r) {
         return 0;
     }
 
@@ -396,8 +354,7 @@ static size_t AdjustedMaxFileSize(const char *file_path, size_t file_size)
 int ConfigStore_Open(ConfigStore *p, const char *base_filepath, size_t max_size, int flags,
                      ConfigStoreReplicaType rtype)
 {
-    if (p->_fd >= 0)
-    {
+    if (p->_fd >= 0) {
         errno = EALREADY;
         return -1;
     }
@@ -406,16 +363,14 @@ int ConfigStore_Open(ConfigStore *p, const char *base_filepath, size_t max_size,
     ConfigStore_Init(&temp);
 
     size_t adjusted_max_size = AdjustedMaxFileSize(base_filepath, max_size);
-    if (adjusted_max_size == 0)
-    {
+    if (adjusted_max_size == 0) {
         errno = ENOSPC;
         return -1;
     }
 
     int res = Impl_Open(&temp, base_filepath, adjusted_max_size, flags, rtype);
 
-    if (res == 0)
-    {
+    if (res == 0) {
         ConfigStore_Move(p, &temp);
     }
 
@@ -426,20 +381,17 @@ int ConfigStore_Open(ConfigStore *p, const char *base_filepath, size_t max_size,
 
 static int Impl_WriteToFile(int fd, ConfigStore *p)
 {
-    if (lseek(fd, 0, SEEK_SET) < 0)
-    {
+    if (lseek(fd, 0, SEEK_SET) < 0) {
         return -1;
     }
 
     ssize_t total_size = p->_end - p->_begin;
 
-    if (write(fd, p->_begin, total_size) != total_size)
-    {
+    if (write(fd, p->_begin, total_size) != total_size) {
         return -1;
     }
 
-    if (ftruncate(fd, total_size) != 0)
-    {
+    if (ftruncate(fd, total_size) != 0) {
         return -1;
     }
 
@@ -450,8 +402,7 @@ static int Impl_WriteToFile(int fd, ConfigStore *p)
 
 int ConfigStore_Commit(ConfigStore *p)
 {
-    if (!ConfigStore_InvariantsCheck(p))
-    {
+    if (!ConfigStore_InvariantsCheck(p)) {
         errno = EINVAL;
         return -1;
     }
@@ -463,37 +414,30 @@ int ConfigStore_Commit(ConfigStore *p)
     ConfigStoreKvpHeader *first = (ConfigStoreKvpHeader *)p->_begin;
     ConfigStoreKvpHeader *last = (ConfigStoreKvpHeader *)p->_end;
 
-    if ((first != last) && (first->key == ConfigStoreFileHeaderKey))
-    {
+    if ((first != last) && (first->key == ConfigStoreFileHeaderKey)) {
         ConfigStoreFileHeader *header = (ConfigStoreFileHeader *)(first);
         header->file_size = (p->_end - p->_begin);
         header->crc = crc;
     }
 
-    if (p->_replica_type == ConfigStoreReplica_Swap)
-    {
+    if (p->_replica_type == ConfigStoreReplica_Swap) {
         // Create the swap file always.
         int fd = open(p->_replica_path, O_RDWR | O_CREAT | O_CLOEXEC | O_TRUNC, S_IRUSR | S_IWUSR);
-        if (fd < 0)
-        {
+        if (fd < 0) {
             return -1;
         }
         int res = Impl_WriteToFile(fd, p);
         close(fd);
-        if (res < 0)
-        {
+        if (res < 0) {
             return -1;
         }
         res = rename(p->_replica_path, p->_primary_path);
-        if (res < 0)
-        {
+        if (res < 0) {
             return -1;
         }
 
         ConfigStore_Close(p);
-    }
-    else
-    {
+    } else {
         return Impl_WriteToFile(p->_fd, p);
     }
 
@@ -515,16 +459,14 @@ ConfigStoreKvpHeader *ConfigStore_InsertKvp(ConfigStore *p, const ConfigStoreKvp
                                             ConfigStoreKey key, size_t size)
 {
     uint16_t kvp_size;
-    if (__builtin_add_overflow(size, sizeof(ConfigStoreKvpHeader), &kvp_size))
-    {
+    if (__builtin_add_overflow(size, sizeof(ConfigStoreKvpHeader), &kvp_size)) {
         return NULL;
     }
 
     size_t in_offset = (ptrdiff_t)pos - (ptrdiff_t)p->_begin;
     size_t current_size = p->_end - p->_begin;
 
-    if (ConfigStore_ReserveCapacity(p, current_size + kvp_size))
-    {
+    if (ConfigStore_ReserveCapacity(p, current_size + kvp_size)) {
         return NULL;
     }
 
@@ -544,8 +486,7 @@ ConfigStoreKvpHeader *ConfigStore_InsertKvp(ConfigStore *p, const ConfigStoreKvp
 static ConfigStoreKvpHeader *Impl_FindKey(ConfigStoreKey key, ConfigStoreKvpHeader *pFirst,
                                           ConfigStoreKvpHeader *pLast)
 {
-    while ((pFirst != pLast) && (pFirst->key != key))
-    {
+    while ((pFirst != pLast) && (pFirst->key != key)) {
         pFirst = ConfigStore_GetNextKvp(pFirst, pLast);
     }
 
@@ -567,10 +508,8 @@ ConfigStoreKvpHeader *ConfigStore_PutUniqueKey(ConfigStore *p, ConfigStoreKey ke
     ConfigStoreKvpHeader *it_end = NULL;
 
     // For all matching keys.
-    while (it_end = ConfigStore_EndKvp(p), it = Impl_FindKey(key, it, it_end), it != it_end)
-    {
-        if (it->size != value_size)
-        {
+    while (it_end = ConfigStore_EndKvp(p), it = Impl_FindKey(key, it, it_end), it != it_end) {
+        if (it->size != value_size) {
             // Not same size. Erase KVP and continue with next.
             it = ConfigStore_EraseKvp(p, it);
             continue;
@@ -580,26 +519,22 @@ ConfigStoreKvpHeader *ConfigStore_PutUniqueKey(ConfigStore *p, ConfigStoreKey ke
         // key after it, just in case.
         ConfigStoreKvpHeader *it_erase = ConfigStore_GetNextKvp(it, it_end);
         while (it_end = ConfigStore_EndKvp(p), it_erase = Impl_FindKey(key, it_erase, it_end),
-               it_erase != it_end)
-        {
+               it_erase != it_end) {
             it_erase = ConfigStore_EraseKvp(p, it_erase);
         }
         break;
     }
 
     it_end = ConfigStore_EndKvp(p);
-    if (it == it_end)
-    {
+    if (it == it_end) {
         it = ConfigStore_InsertKvp(p, it_end, key, value_size);
-        if (it == ConfigStore_EndKvp(p))
-        {
+        if (it == ConfigStore_EndKvp(p)) {
             // Space exhaustion.
             return NULL;
         }
     }
 
-    if (optional_data != NULL)
-    {
+    if (optional_data != NULL) {
         ConfigStore_WriteValue(it, 0, optional_data, value_size);
     }
 
@@ -621,36 +556,30 @@ ConfigStoreKvpHeader *ConfigStore_AllocUniqueKvp(ConfigStore *p, ConfigStoreKey 
                                                  ConfigStoreKey last_key, size_t value_size,
                                                  ConfigStoreKey key_increment)
 {
-    while (first_key < last_key)
-    {
+    while (first_key < last_key) {
 
         bool found = false;
 
         ConfigStoreKvpHeader *kvp = ConfigStore_BeginKvp(p);
-        while (kvp != ConfigStore_EndKvp(p))
-        {
+        while (kvp != ConfigStore_EndKvp(p)) {
             found = (kvp->key == first_key);
-            if (found)
-            {
+            if (found) {
                 break;
             }
             kvp = ConfigStore_GetNextKvp(kvp, ConfigStore_EndKvp(p));
         }
 
-        if (!found)
-        {
+        if (!found) {
             break;
         }
 
-        if (__builtin_add_overflow(first_key, key_increment, &first_key))
-        {
+        if (__builtin_add_overflow(first_key, key_increment, &first_key)) {
             errno = ENOENT;
             return NULL;
         }
     }
 
-    if (first_key >= last_key)
-    {
+    if (first_key >= last_key) {
         errno = ENOENT;
         return NULL;
     }
@@ -662,23 +591,18 @@ int ConfigStore_EraseKeysInRange(ConfigStore *p, ConfigStoreKey first_key, Confi
                                  ConfigStoreKey key_increment)
 {
     bool good_args = (p) && (first_key <= last_key) && (1 <= key_increment);
-    if (!good_args)
-    {
+    if (!good_args) {
         errno = EINVAL;
         return -1;
     }
 
     ConfigStoreKvpHeader *kvp = ConfigStore_BeginKvp(p);
-    while (kvp != ConfigStore_EndKvp(p))
-    {
+    while (kvp != ConfigStore_EndKvp(p)) {
         bool match = (first_key <= kvp->key) && (kvp->key < last_key) &&
                      (((kvp->key - first_key) % key_increment) == 0);
-        if (match)
-        {
+        if (match) {
             kvp = ConfigStore_EraseKvp(p, kvp);
-        }
-        else
-        {
+        } else {
             kvp = ConfigStore_GetNextKvp(kvp, ConfigStore_EndKvp(p));
         }
     }
@@ -695,12 +619,10 @@ ConfigStoreKvpHeader *ConfigStore_GetNextKvpInRange(ConfigStore *p, const Config
 
     pos = pos ? ConfigStore_GetNextKvp(pos, end_pos) : ConfigStore_BeginKvp(p);
 
-    while (pos != end_pos)
-    {
+    while (pos != end_pos) {
         bool match = (first_key <= pos->key) && (pos->key < last_key) &&
                      (((pos->key - first_key) % key_increment) == 0);
-        if (match)
-        {
+        if (match) {
             break;
         }
         pos = ConfigStore_GetNextKvp(pos, end_pos);
@@ -717,8 +639,7 @@ int ConfigStore_WriteValue(ConfigStoreKvpHeader *pos, size_t offset, const void 
     uint8_t *dst_data = (uint8_t *)pos + hdr_size + offset;
 
     size_t last_offset = offset + size;
-    if (dst_size < last_offset)
-    {
+    if (dst_size < last_offset) {
         errno = E2BIG;
         return -1;
     }
@@ -738,8 +659,7 @@ size_t ConfigStore_ValidateFormat(const uint8_t *data, size_t size)
                       (first->key == ConfigStoreFileHeaderKey) &&
                       (first->size >= sizeof(ConfigStoreFileHeader));
 
-    if (!has_header)
-    {
+    if (!has_header) {
         return 0;
     }
 
@@ -748,8 +668,7 @@ size_t ConfigStore_ValidateFormat(const uint8_t *data, size_t size)
     bool ok = (header->signature == ConfigStoreFileSignature) &&
               (header->version == ConfigStoreFileVersion) &&
               (header->header.size <= header->file_size) && (header->file_size <= size);
-    if (!ok)
-    {
+    if (!ok) {
         return 0;
     }
 
@@ -760,17 +679,14 @@ size_t ConfigStore_ValidateFormat(const uint8_t *data, size_t size)
 
     uint32_t crc = ConfigStore_AddCrc(ConfigStoreCrcInitValue, data, size);
 
-    if (crc != header->crc)
-    {
+    if (crc != header->crc) {
         return 0;
     }
 
     ++first;
 
-    while ((first != NULL) && (first != last))
-    {
-        if (first->key == ConfigStoreFileHeaderKey)
-        {
+    while ((first != NULL) && (first != last)) {
+        if (first->key == ConfigStoreFileHeaderKey) {
             // The header key must only be used in the beginning of the file.
             break;
         }
@@ -778,8 +694,7 @@ size_t ConfigStore_ValidateFormat(const uint8_t *data, size_t size)
         first = ConfigStore_GetNextKvp(first, last);
     }
 
-    if (first != last)
-    {
+    if (first != last) {
         // Didn't get to the end of the file.
         return 0;
     }
